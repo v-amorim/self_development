0e8c04046e6fc2fea901c8d731fdae1ebd416176
Vinícius Amorim <vamorim.dev@gmail.com>
2022-10-04 11:48:55 -0300

    alura py_adv 02.01

--- python/alura/forca.py
+++ python/alura/forca.py
@@ -6,7 +6,15 @@ def jogar():
     palavra_secreta = "banana"
     enforcou = False
     acertou = False
-    while(not enforcou and not acertou):
+    while (not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.lower() == letra.lower()):
+                print(f"Encontrei a letra [{letra.upper()}] na posição [{index}]")
+
+            index = index + 1
+
         print("Jogando...")
 
     print("Fim do jogo")

c1de9954411a504e699a41b6681ffe29043597d0
Vinícius Amorim <vamorim.dev@gmail.com>
2022-10-17 10:35:24 -0300

    alura py_oo 02.01

--- python/alura/oo/conta.py
+++ python/alura/oo/conta.py
@@ -1,16 +1,5 @@
-def cria_conta(numero, titular, saldo, limite):
-    return {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
 
 
-def deposita(conta, valor):
-    conta["saldo"] += valor
-
-
-def saca(conta, valor):
-    conta["saldo"] -= valor
-
-
-def extrato(conta):
-    print(f'Saldo é {conta["saldo"]}')
-
-
+class Conta:
+    
+    pass
---
+++ python/alura/oo/teste.py
@@ -0,0 +1,16 @@
+def cria_conta(numero, titular, saldo, limite):
+    return {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+
+def extrato(conta):
+    print(f'Saldo é {conta["saldo"]}')
+
+

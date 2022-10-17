955a24c87726fb055ce8be15a97cfa779612ee51
Vinícius Amorim <vamorim.dev@gmail.com>
2022-10-17 13:54:04 -0300

    alura py_oo 04.01

--- python/alura/oo/conta.py
+++ python/alura/oo/conta.py
@@ -1,19 +1,19 @@
 
 class Conta:
     def __init__(self, numero, titular, saldo, limite=1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'Saldo de {self.titular} é {self.saldo}')
+        print(f'Saldo de {self.__titular} é {self.__saldo}')
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
     def transfere(self, valor, destino):
         self.saca(valor)

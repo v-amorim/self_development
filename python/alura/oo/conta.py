7207c0e57003fe977abdaf3bc8b29701e35a26c2
Vinícius Amorim <vamorim.dev@gmail.com>
2022-10-17 10:57:15 -0300

    alura py_oo 02.06

--- python/alura/oo/conta.py
+++ python/alura/oo/conta.py
@@ -1,6 +1,6 @@
 
 class Conta:
-    def __init__(self, numero, titular, saldo, limite):
+    def __init__(self, numero, titular, saldo, limite=1000.0):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo

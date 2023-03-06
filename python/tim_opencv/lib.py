572c7ebd4202fd6dad878505ca303f755329204b
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-06 15:55:06 -0300

    lib

--- python/tim_opencv/lib.py
+++ python/tim_opencv/lib.py
@@ -6,58 +6,42 @@ COLOR_BGR2GRAY = cv2.COLOR_BGR2GRAY
 FILLER_IMG = [np.ones((3, 3, 3), dtype=np.uint8) * 255]
 
 
-def show_image(image, ispath=True):
-    if ispath:
-        img = cv2.imread(image[0])
-        color_conversion = image[2] if len(image) > 2 else cv2.COLOR_BGR2RGB
-        img = cv2.cvtColor(img, color_conversion) if len(image) > 2 else img
-    else:
-        img = image[0]
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    if len(image) > 2 and image[2] == COLOR_BGR2GRAY:
-        plt.imshow(img, cmap='gray')
-    else:
-        plt.imshow(img)
-
-    if len(image) > 1 and image[1]:
-        plt.title(image[1])
-
-    plt.axis('off')
-
-    plt.tight_layout()
-    plt.show()
-
-
-def show_images(images, ispath=True):
-    num_images = len(images)
+def create_figure(num_images, figsize):
     num_cols = min(3, num_images)
     num_rows = (num_images + num_cols - 1) // num_cols
-    figsize = (num_cols * 5, num_rows * 5)
-
+    figsize = figsize or (num_cols * 5, num_rows * 5)
     fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
-    axes = axes.flatten()
+    axes = np.ravel(axes)
+    return fig, axes
 
-    for i in range(num_images):
-        if ispath:
-            img = cv2.imread(images[i][0])
-            color_conversion = images[i][2] if len(images[i]) > 2 else cv2.COLOR_BGR2RGB
-            img = cv2.cvtColor(img, color_conversion) if len(images[i]) > 2 else img
-        else:
-            img = images[i][0]
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        if len(images[i]) > 2 and images[i][2] == COLOR_BGR2GRAY:
-            axes[i].imshow(img, cmap='gray')
-        else:
-            axes[i].imshow(img)
+def show_image(ax, img, is_grayscale=False, title=None):
+    cmap = 'gray' if is_grayscale else None
+    ax.imshow(img.astype('uint8'), cmap=cmap)
+    if title:
+        ax.set_title(title)
+    ax.axis('off')
 
-        if len(images[i]) > 1 and images[i][1]:
-            axes[i].set_title(images[i][1])
-        axes[i].axis('off')
 
-    for i in range(num_images, num_rows * num_cols):
-        fig.delaxes(axes[i])
+def show_images(images, figsize=None):
+    fig, axes = create_figure(len(images), figsize)
+    for i, image in enumerate(images):
+        _2param = len(image) > 2
+        _1param = len(image) > 1
+        img = cv2.imread(image[0]) if isinstance(image[0], str) else image[0]
+        conversion = image[2] if _2param else image[1] if _1param and type(image[1]) == int else cv2.COLOR_BGR2RGB
+
+        if conversion is not None:
+            img = cv2.cvtColor(img, conversion)
+
+        is_grayscale = conversion == cv2.COLOR_BGR2GRAY
+        title = image[1] if _1param and type(image[1]) == str else None
+
+        show_image(axes.flat[i], img, is_grayscale=is_grayscale, title=title)
+
+    if len(images) < len(axes):
+        for i in range(len(images), len(axes)):
+            axes.flat[i].axis('off')
 
     plt.tight_layout()
     plt.show()

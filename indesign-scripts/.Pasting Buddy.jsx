3c18c5923084fe86d0b3dbcb904424abd900b11b
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-07 11:53:01 -0300

    changed

--- indesign-scripts/.Pasting Buddy.jsx
+++ indesign-scripts/.Pasting Buddy.jsx
@@ -40,34 +40,31 @@ formattingButton.onClick = function () {
     formattingButton.text = paste_without_formatting ? "Paste without formatting" : "Paste with formatting";
 };
 
-
-// ---
-
 var doc = app.activeDocument;
 var master_frame = app.selection[0]; // <-- the Text Script frame
 
-if (master_frame instanceof TextFrame || master_frame instanceof Story) {
-    dialog.show();
-    app.selection = null;
-    doc.addEventListener('afterSelectionChanged', selectionChanged);
-} else {
-    alert("Please, select a frame with text before run the script!");
-    exit();
+function init() {
+    if (master_frame instanceof TextFrame || master_frame instanceof Story) {
+        dialog.show();
+        app.selection = null;
+        doc.addEventListener('afterSelectionChanged', selectionChanged);
+    } else {
+        alert("Please, select a frame with text before run the script!");
+        exit();
+    }
 }
 
 function selectionChanged() {
-
     if (pause) return;
 
     var sel = doc.selection[0];
 
     if (sel instanceof InsertionPoint) sel = sel.parent.textContainers[0];
     if (sel instanceof TextFrame || sel instanceof Story) {
-
         if (sel.contents == '' && master_frame.contents != '') {
-
             app.select(master_frame.paragraphs[0], SelectionOptions.REPLACE_WITH);
-            textEdit.text = master_frame.paragraphs[0].contents;
+            nextText = master_frame.paragraphs[1] ? master_frame.paragraphs[1].contents : "";
+            textEdit.text = nextText;
             app.cut();
             app.selection = null;
             sel.texts.everyItem().select();
@@ -78,13 +75,20 @@ function selectionChanged() {
             }
             app.selection = null;
 
-            // remove '\r' at the end
-            app.findGrepPreferences.findWhat = "\r$";
-            app.changeGrepPreferences.changeTo = "";
-            sel.changeGrep();
-
+            removeLineBreaks(sel);
+            sel.parentStory.changeGrep();
         }
 
         if (master_frame.contents == '') alert("No text to paste anymore!")
     }
 }
+
+function removeLineBreaks(sel) {
+    // remove '\r' at the end
+    app.findGrepPreferences.findWhat = "\r$";
+    app.changeGrepPreferences.changeTo = "";
+    sel.changeGrep();
+    sel.parentStory.changeGrep();
+}
+
+init();

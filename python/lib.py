965020177d46f6a7e7bbb72e11492515a7aa98d6
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-11 01:14:10 -0300

    lib: added show_video and edited show_images

---
+++ python/lib.py
@@ -0,0 +1,97 @@
+import cv2
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+
+COLOR_BGR2GRAY = cv2.COLOR_BGR2GRAY
+FILLER_IMG = [np.ones((3, 3, 3), dtype=np.uint8) * 255]
+
+
+def create_plot(num_images, figsize):
+    num_cols = min(3, num_images)
+    num_rows = (num_images + num_cols - 1) // num_cols
+    figsize = figsize or (num_cols * 5, num_rows * 5)
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = np.ravel(axes)
+    return fig, axes
+
+
+def add_image_to_plot(ax, img, is_grayscale=False, title=None):
+    cmap = 'gray' if is_grayscale else None
+    ax.imshow(img.astype('uint8'), cmap=cmap)
+    if title:
+        ax.set_title(title)
+    ax.axis('off')
+
+
+def show_single_image(image, figsize=(10, 10)):
+    plt.figure(figsize=figsize)
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) if len(image.shape) == 3 else image
+    plt.imshow(image, cmap='gray') if len(image.shape) == 2 else plt.imshow(image)
+    plt.title(f'{image.shape}')
+
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.show()
+
+
+def show_images(images, figsize=None):
+    if type(images) == np.ndarray:
+        show_single_image(images, figsize)
+        return
+    else:
+        images = [images] if type(images[0]) != list else images
+
+    fig, axes = create_plot(len(images), figsize)
+    for i, image in enumerate(images):
+        _2param = len(image) > 2
+        _1param = len(image) > 1
+        img = cv2.imread(image[0]) if isinstance(image[0], str) else image[0]
+        conversion = image[2] if _2param else image[1] if _1param and type(image[1]) == int else cv2.COLOR_BGR2RGB
+
+        if conversion is not None and len(img.shape) == 3:
+            img = cv2.cvtColor(img, conversion)
+
+        is_grayscale = conversion == cv2.COLOR_BGR2GRAY or len(img.shape) == 2
+        title = image[1] if _1param and type(image[1]) == str else None
+
+        add_image_to_plot(axes.flat[i], img, is_grayscale=is_grayscale, title=title)
+
+    if len(images) < len(axes):
+        for i in range(len(images), len(axes)):
+            axes.flat[i].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def show_video(video=None, frame_width=None, frame_height=None):
+    if video is not None:
+        video_path = video[0]
+        video_title = video[1]
+    else:
+        video_path = 0
+        video_title = 'Video'
+
+    cap = cv2.VideoCapture(video_path)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        time.sleep(1 / fps)
+
+        if frame_width is not None and frame_height is not None:
+            frame = cv2.resize(frame, (frame_width, frame_height))
+
+        cv2.imshow(video_title, frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()

d5106dcafbf12e0910cebff938152bfde944d143
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-24 06:54:55 -0300

    refactored aliases

--- macros/Profile.ps1
+++ macros/Profile.ps1
@@ -1,85 +1,48 @@
-# List all used aliases.
-function h { Get-History }
+##--- General shorcuts
+    function alias  { "Use -<command> for more description: [alias],[grep],[h],[ls],[s],[..],[...],[....],[.....],[mkdir],[p],[pe311],[pe38],[pe],[pf],[pm],[pp],[a],[d],[ga],[gc],[gcl],[gco],[gd],[gf],[gforce],[gl],[glog],[gp],[gr],[gs],[greset]" }
+    function -alias { "List all aliases" }
+    function grep   { Select-String -Path $args }   ; function -grep   { "Search for text within files [find /i <text>]" }
+    function h      { Get-History }                 ; function -h      { "List all used aliases [DOSKEY /HISTORY]" }
+    function ls     { Get-ChildItem $args }         ; function -ls     { "List files and folders in the current directory [DIR <file>]" }
+    function s      { Invoke-Item . }               ; function -s      { "Open File Explorer in the current directory [start .]" }
+    function ..     { Set-Location .. }             ; function -..     { "Go up one directory [cd ..]" }
+    function ...    { Set-Location ..\.. }          ; function -...    { "Go up two directories [cd ..\..]" }
+    function ....   { Set-Location ..\..\.. }       ; function -....   { "Go up three directories [cd ..\..\..]" }
+    function .....  { Set-Location ..\..\..\.. }    ; function -.....  { "Go up four directories [cd ..\..\..\..]" }
+    function cls    { Clear-Host }                  ; function -cls    { "Clear the console [cls]" }
+    function mkdir  { New-Item -ItemType Directory $args[0] | Set-Location } ; function -mkdir { "Create a new directory and navigate to it [mkdir <directory> && cd <directory>]" }
 
-# List all available aliases.
-function alias { Get-Alias }
+##--- Python shortcuts
+    function p      { python $args }                ; function -p      { "Run a Python script [python <script.py>]" }
+    function pe     { pyenv $args }                 ; function -pe     { "Activate Python 3.11.2 on the current shell [pyenv shell 3.11.2]" }
+    function pe311  { pyenv shell 3.11.2 }          ; function -pe311  { "Activate Python 3.8.10 on the current shell [pyenv shell 3.8.10]" }
+    function pe38   { pyenv shell 3.8.10 }          ; function -pe38   { "Manage Python versions [pyenv <command>]" }
+    function pf     { python -m pip freeze }        ; function -pf     { "List all installed packages [python -m pip freeze]" }
+    function pm     { python -m $args }             ; function -pm     { "Run a Python module [python -m <module>]" }
+    function pp     { python -m pip install $args } ; function -pp     { "Install a Python package [python -m pip install <package>]" }
 
-# General shortcuts
-# List files and folders in the current directory.
-function ls { Get-ChildItem $args }
+##--- Virtual environment shortcuts
+    function a  { & .venv\Scripts\activate.ps1 }    ; function -a      { "Activate the virtual environment [.venv\Scripts\activate.bat]" }
+    function d  { deactivate }                      ; function -d      { "Deactivate the virtual environment [deactivate]" }
 
-# Create a new directory and navigate to it.
-function mkdir { New-Item -ItemType Directory $args[0] | Set-Location }
+##--- Pre-commit shortcuts
+    function pc     { pre-commit $* }               ; DOSKEY -pc       { "Run pre-commit hooks [pre-commit <command>]" }
+    function pci    { pre-commit install }          ; DOSKEY -pci      { "Install pre-commit hooks [pre-commit install]" }
+    function pcall  { pre-commit run --all-files }  ; DOSKEY -pcall    { "Run pre-commit hooks on all files [pre-commit run --all-files]" }
 
-# Search for text within files.
-function grep { Select-String -Path $args }
-
-# Open File Explorer in the current directory.
-function s { Invoke-Item . }
-
-# Go up N directories.
-function ..     { Set-Location .. }
-function ...    { Set-Location ..\.. }
-function ....   { Set-Location ..\..\.. }
-function .....  { Set-Location ..\..\..\.. }
-
-# Go to / directory
-function / { Set-Location / }
-
-# Python shortcuts
-function p      { python $args }
-function pe     { pyenv $args }
-function pe311  { pyenv shell 3.11.2 }
-function pe38   { pyenv shell 3.8.10 }
-function pm     { python -m $args }
-function pp     { python -m pip install $args }
+##--- Git shortcuts
+    function ga     { git add $args }               ; function -ga     { "Stage changes for commit [git add <file>]" }
+    function gc     { git commit -m "$args" }       ; function -gc     { "Commit changes with a message [git commit -m '<message>']" }
+    function gcl    { git clone $args }             ; function -gcl    { "Clone a repository [git clone <url>]" }
+    function gco    { git checkout $args }          ; function -gco    { "Switch to a different branch or commit [git checkout <branch>]" }
+    function gd     { git diff $args }              ; function -gd     { "See the difference between the working directory and the staging area [git diff <file>]" }
+    function gf     {git fetch}                     ; function -gf     { "Fetch changes from the remote repository [git fetch]" }
+    function gforce {git push -f}                   ; function -gforce { "Force push to the current branch [git push -f]" }
+    function gl     { git log $args }               ; function -gl     { "See the commit history of the repository [git log <file>]" }
+    function glog   {git log --oneline}             ; function -glog   { "Get one-line summary of each commit [git log --oneline]" }
+    function gp     { git push }                    ; function -gp     { "Push changes to the remote repository [git push]" }
+    function gr     { git remote $args }            ; function -gr     { "Manage the remote repositories [git remote <command>]" }
+    function greset {git reset --hard origin/main}  ; function -greset { "See the current status of the repository [git status]" }
+    function gs     { git status }                  ; function -gs     { "Reset the current branch to the origin/main branch [git reset --hard origin/main]" }
 
 
-# Activate the virtual environment.
-function a  { & .venv\Scripts\activate.ps1 }
-
-# Deactivate the virtual environment.
-function d { deactivate }
-
-# Git shortcuts
-# Clone a repository.
-function gcl { git clone $args }
-
-# Stage changes for commit.
-function ga { git add $args }
-
-# Commit changes with a message.
-function gc { git commit -m "$args" }
-
-# Push changes to the remote repository.
-function gp { git push }
-
-# Switch to a different branch or commit.
-function gco { git checkout $args }
-
-# See the current status of the repository.
-function gs { git status }
-
-# See the difference between the working directory and the staging area.
-function gd { git diff $args }
-
-# See the commit history of the repository.
-function gl { git log $args }
-
-# Manage the remote repositories.
-function gr { git remote $args }
-
-# Open the Git repository visualizer in a separate window.
-function gitk { Start-Process gitk -ArgumentList "--all" }
-
-# Fetch changes from the remote repository.
-function gf {git fetch}
-
-# Reset the current branch to the origin/main branch.
-function greset {git reset --hard origin/main}
-
-# Force push to the current branch.
-function gforce {git push -f}
-
-# Clear the screen.
-function cls { Clear-Host }
--- macros/alias.bat
+++ macros/alias.bat
@@ -1,88 +1,51 @@
-:: DOSKEY shortcuts, https://stackoverflow.com/questions/20530996/aliases-in-windows-command-prompt
-:: To add to Windows Terminal, add a `/k path/to/alias.bat` to the end of the `commandline` property on the [Settings>Profiles>Command Prompt] (Ctrl+,) page.
-:: List all used aliases.
-DOSKEY h=DOSKEY /HISTORY
+::  DOSKEY shortcuts, https://stackoverflow.com/questions/20530996/aliases-in-windows-command-prompt
+::  To add to Windows Terminal, add a `/k path/to/alias.bat` to the end of the `commandline` property on the [Settings>Profiles>Command Prompt] (Ctrl+,) page.
 
-:: List all available aliases.
-DOSKEY alias=DOSKEY /macros
-
-
-:: General shortcuts
-:: List files and folders in the current directory.
-DOSKEY ls=DIR $* 
-
-:: Create a new directory and navigate to it.
-DOSKEY mkdir=mkdir $* && cd $*
-
-:: Search for text within files.
-DOSKEY grep=find /i "$*"
-
-:: Open File Explorer in the current directory.
-DOSKEY s=start .
-
-:: Go up N directories.
-DOSKEY ..=cd ..
-DOSKEY ...=cd ..\..
-DOSKEY ....=cd ..\..\..
-DOSKEY .....=cd ..\..\..\..
-
-:: Go to / directory
-DOSKEY /=cd /
+::  General shortcuts
+    DOSKEY alias=   echo "Use -<command> for more description: [alias],[grep],[h],[ls],[s],[..],[...],[....],[.....],[mkdir],[p],[pe311],[pe38],[pe],[pf],[pm],[pp],[a],[d],[ga],[gc],[gcl],[gco],[gd],[gf],[gforce],[gl],[glog],[gp],[gr],[gs],[greset]"
+    DOSKEY -alias=  echo "List all available aliases"
+    DOSKEY grep=find /i "$*"                    & DOSKEY -grep=     echo "Search for text within files [find /i <text>]"
+    DOSKEY h=DOSKEY /HISTORY                    & DOSKEY -h=        echo "List all used aliases [DOSKEY /HISTORY]"
+    DOSKEY ls=DIR $*                            & DOSKEY -ls=       echo "List files and folders in the current directory [DIR <file>]"
+    DOSKEY s=start .                            & DOSKEY -s=        echo "Open File Explorer in the current directory [start .]"
+    DOSKEY ..=cd ..                             & DOSKEY -..=       echo "Go up one directory [cd ..]"
+    DOSKEY ...=cd ..\..                         & DOSKEY -...=      echo "Go up two directories [cd ..\..]"
+    DOSKEY ....=cd ..\..\..                     & DOSKEY -....=     echo "Go up three directories [cd ..\..\..]"
+    DOSKEY .....=cd ..\..\..\..                 & DOSKEY -.....=    echo "Go up four directories [cd ..\..\..\..]"
+    DOSKEY mkdir=mkdir $* && cd $*              & DOSKEY -mkdir=    echo "Create a new directory and navigate to it [mkdir <directory> && cd <directory>]"
 
 :: Python shortcuts
-DOSKEY p=python $*
-DOSKEY pe=pyenv $*
-DOSKEY pe311=pyenv shell 3.11.2
-DOSKEY pe38=pyenv shell 3.8.10
-DOSKEY pm=python -m $*
-DOSKEY pp=python -m pip install $*
+    DOSKEY p=python $*                          & DOSKEY -p=        echo "Run a Python script [python <script.py>]"
+    DOSKEY pe311=pyenv shell 3.11.2             & DOSKEY -pe311=    echo "Activate Python 3.11.2 on the current shell [pyenv shell 3.11.2]"
+    DOSKEY pe38=pyenv shell 3.8.10              & DOSKEY -pe38=     echo "Activate Python 3.8.10 on the current shell [pyenv shell 3.8.10]"
+    DOSKEY pe=pyenv $*                          & DOSKEY -pe=       echo "Manage Python versions [pyenv <command>]"
+    DOSKEY pf=python -m pip freeze              & DOSKEY -pf=       echo "List all installed packages [python -m pip freeze]"
+    DOSKEY pm=python -m $*                      & DOSKEY -pm=       echo "Run a Python module [python -m <module>]"
+    DOSKEY pp=python -m pip install $*          & DOSKEY -pp=       echo "Install a Python package [python -m pip install <package>]"
 
-:: Activate the virtual environment.
-DOSKEY a=.venv\Scripts\activate.bat
+:: Virtual environment shortcuts
+    DOSKEY a=.venv\Scripts\activate.bat         & DOSKEY -a=        echo "Activate the virtual environment [.venv\Scripts\activate.bat]"
+    DOSKEY d=deactivate                         & DOSKEY -d=        echo "Deactivate the virtual environment [deactivate]"
 
-:: Deactivate the virtual environment.
-DOSKEY d=deactivate
+:: Pre-commit shortcut
+    DOSKEY pc=pre-commit $*                     & DOSKEY -pc=       echo "Run pre-commit hooks [pre-commit <command>]"
+    DOSKEY pci=pre-commit install               & DOSKEY -pci=      echo "Install pre-commit hooks [pre-commit install]"
+    DOSKEY pcall=pre-commit run --all-files     & DOSKEY -pcall=    echo "Run pre-commit hooks on all files [pre-commit run --all-files]"
 
 
 :: Git shortcuts
-:: Clone a repository.
-DOSKEY gcl=git clone $*
+    DOSKEY ga=git add $*                        & DOSKEY -ga=       echo "Stage changes for commit [git add <file>]"
+    DOSKEY gc=git commit -m "$*"                & DOSKEY -gc=       echo "Commit changes with a message [git commit -m '<message>']"
+    DOSKEY gcl=git clone $*                     & DOSKEY -gcl=      echo "Clone a repository [git clone <url>]"
+    DOSKEY gco=git checkout $*                  & DOSKEY -gco=      echo "Switch to a different branch or commit [git checkout <branch>]"
+    DOSKEY gd=git diff $*                       & DOSKEY -gd=       echo "See the difference between the working directory and the staging area [git diff <file>]"
+    DOSKEY gf=git fetch                         & DOSKEY -gf=       echo "Fetch changes from the remote repository [git fetch]"
+    DOSKEY gforce=git push -f                   & DOSKEY -gforce=   echo "Force push to the current branch [git push -f]"
+    DOSKEY gl=git log $*                        & DOSKEY -gl=       echo "See the commit history of the repository [git log <file>]"
+    DOSKEY glog=git log --oneline               & DOSKEY -glog=     echo "Get one-line summary of each commit [git log --oneline]"
+    DOSKEY gp=git push                          & DOSKEY -gp=       echo "Push changes to the remote repository [git push]"
+    DOSKEY gr=git remote $*                     & DOSKEY -gr=       echo "Manage the remote repositories [git remote <command>]"
+    DOSKEY gs=git status                        & DOSKEY -gs=       echo "See the current status of the repository [git status]"
+    DOSKEY greset=git reset --hard origin/main  & DOSKEY -greset=   echo "Reset the current branch to the origin/main branch [git reset --hard origin/main]"
 
-:: Stage changes for commit.
-DOSKEY ga=git add $*
-
-:: Commit changes with a message.
-DOSKEY gc=git commit -m "$*"
-
-:: Push changes to the remote repository.
-DOSKEY gp=git push
-
-:: Switch to a different branch or commit.
-DOSKEY gco=git checkout $*
-
-:: See the current status of the repository.
-DOSKEY gs=git status
-
-:: See the difference between the working directory and the staging area.
-DOSKEY gd=git diff $*
-
-:: See the commit history of the repository.
-DOSKEY gl=git log $*
-
-:: Manage the remote repositories.
-DOSKEY gr=git remote $*
-
-:: Open the Git repository visualizer in a separate window.
-DOSKEY gitk=gitk --all&
-
-:: Fetch changes from the remote repository.
-DOSKEY gf=git fetch
-
-:: Reset the current branch to the origin/main branch.
-DOSKEY greset=git reset --hard origin/main
-
-:: Force push to the current branch.
-DOSKEY gforce=git push -f
-
-:: Clear the screen.
 cls

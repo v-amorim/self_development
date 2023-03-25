c86192733e4f602d4e80e4e211d1d99cc06809eb
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-25 11:37:21 -0300

    feat: extract graph

--- python/miui_mindmap_to_md/structure.py
+++ python/miui_mindmap_to_md/structure.py
@@ -16,8 +16,28 @@ for i in range(len(divs)):
     div = re.sub(r'\n\s*', ' ', div)
     divs[i] = div
 
+# Create a dictionary to store the graph
+graph = {}
 
-# Join the divs into a single string and write to a markdown file
-markdown = "\n\n".join(divs)
-with open(f"{FILENAME}.md", "w", encoding='utf-8') as f:
-    f.write(markdown)
+# Create a function to add nodes to the graph
+
+
+def add_node(node_id, parent_id, text):
+    if node_id not in graph:
+        graph[node_id] = {'text': text, 'children': []}
+    if parent_id is not None:
+        if parent_id not in graph:
+            graph[parent_id] = {'text': '', 'children': []}
+        graph[parent_id]['children'].append(node_id)
+
+
+# Add nodes to the graph
+for div in divs:
+    node_id = re.search(r'id="(.*?)"', div).group(1)
+    parent_id = re.search(r'data-parent="(.*?)"', div)
+    parent_id = parent_id.group(1) if parent_id else None
+    text = re.sub(r'<[^>]*>', '', div)
+    add_node(node_id, parent_id, text)
+
+# Print the graph
+print(graph)

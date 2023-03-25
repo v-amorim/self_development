c283bdf78d035f59e39a6c482fb44cc80d79b3d0
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-25 13:57:39 -0300

    feat: working as intended

--- python/miui_mindmap_to_md/structure.py
+++ python/miui_mindmap_to_md/structure.py
@@ -1,6 +1,6 @@
 import re
 
-FILENAME = "aa"
+FILENAME = "structure"
 
 # Read the contents of the HTML file
 with open(f"{FILENAME}.html", "r", encoding='utf-8') as f:
@@ -24,10 +24,30 @@ def add_node(node_id, parent_id, text):
     # Create a function to add nodes to the graph
     if node_id not in graph:
         graph[node_id] = {'text': text, 'children': []}
-    if parent_id is not None and parent_id != 'root':
+    if parent_id is not None:
         if parent_id not in graph:
             graph[parent_id] = {'text': '', 'children': []}
         graph[parent_id]['children'].append(node_id)
+    else:
+        # Create root node if it doesn't already exist
+        if 'root' not in graph:
+            graph['root'] = {'text': '', 'children': []}
+        graph['root']['children'].append(node_id)
+
+
+def dfs(node_id, depth=0):
+    # Depth-first search to traverse the tree and create bullet points
+    node = graph[node_id]
+    text = node['text']
+    if depth == 1:
+        with open(f"{FILENAME}.md", "w", encoding='utf-8') as f:
+            f.write(f"# {text}\n")
+    else:
+        with open(f"{FILENAME}.md", "a", encoding='utf-8') as f:
+            indent = "  " * (depth - 1)
+            f.write(f"{indent}- {text}\n")
+    for child_id in node['children']:
+        dfs(child_id, depth + 1)
 
 
 # Add nodes to the graph
@@ -36,8 +56,8 @@ for div in divs:
     parent_id = re.search(r'data-parent="(.*?)"', div)
     parent_id = parent_id[1] if parent_id else None
     text = re.sub(r'<[^>]*>', '', div)
+    text = text.strip()
     add_node(node_id, parent_id, text)
 
-# Print the graph
-for index, (key, value) in enumerate(graph.items()):
-    print(index + 1, '\t', key, ' : ', value)
+# Traverse the graph with DFS and output the bullet points to a markdown file
+dfs('root')

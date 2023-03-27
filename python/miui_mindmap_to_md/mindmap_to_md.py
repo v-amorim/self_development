6a8cd810b23050af55e0ef122c5088c76ee7ddd3
Vinícius Amorim <vamorim.dev@gmail.com>
2023-03-26 22:56:32 -0300

    refactor and split into two classes

--- python/miui_mindmap_to_md/mindmap_to_md.py
+++ python/miui_mindmap_to_md/mindmap_to_md.py
@@ -1,67 +1,6 @@
-import re
 import tempfile
 import shutil
 from bs4 import BeautifulSoup
-
-
-class HTMLMindmaptoMDConverter:
-    def __init__(self, filename):
-        self.filename = filename
-        self.html = self.read_file()
-        self.divs = self.get_divs()
-        self.graph = {}
-        self.temp_file = self.create_temp_file()
-
-    def read_file(self):
-        with open(f"{self.filename}.html", "r", encoding='utf-8') as f:
-            return f.read()
-
-    def get_divs(self):
-        pattern = r'<div[^>]+?contenteditable="false"[^>]*?>.*?<\/div>'
-        return re.findall(pattern, self.html, re.DOTALL)
-
-    def clean_div(self, div):
-        div = re.sub(r'\s+contenteditable="false"', '', div)
-        div = re.sub(r'\n\s*', ' ', div)
-        return div
-
-    def add_node(self, node_id, parent_id, text):
-        if node_id not in self.graph:
-            self.graph[node_id] = {'text': text, 'children': []}
-        if parent_id is not None:
-            if parent_id not in self.graph:
-                self.graph[parent_id] = {'text': '', 'children': []}
-            self.graph[parent_id]['children'].append(node_id)
-
-    def dfs(self, node_id, depth=0, is_root=False):
-        node = self.graph[node_id]
-        text = node['text']
-        with open(self.temp_file, "a", encoding='utf-8') as f:
-            if is_root:
-                if node_id == self.graph['root']['children'][0]:
-                    f.write(f"# {text}\n\n")
-                if node_id in self.graph['root']['children'][1:]:
-                    f.write(f"\n# {text}\n\n")
-            elif text != '':
-                indent = "  " * (depth - 2)
-                f.write(f"{indent}- {text}\n")
-        for child_id in node['children']:
-            self.dfs(child_id, depth + 1, is_root=(node_id == 'root'))
-
-    def create_temp_file(self):
-        return tempfile.NamedTemporaryFile(mode='w', delete=False).name
-
-    def convert(self):
-        for div in self.divs:
-            node_id = re.search(r'id="(.*?)"', div)[1]
-            parent_id = re.search(r'data-parent="(.*?)"', div)
-            parent_id = parent_id[1] if parent_id else None
-            text = re.sub(r'<[^>]*>', '', self.clean_div(div))
-            text = text.strip()
-            self.add_node(node_id, parent_id, text)
-
-        self.dfs('root')
-        shutil.move(self.temp_file, f"{self.filename}.md")
 
 
 class HtmlParser:
@@ -78,7 +17,6 @@ class HtmlParser:
         editable_divs = self.soup.find_all(
             'div', {'id': True, 'data-parent': True, 'class': True, 'contenteditable': True})
         for editable_div in editable_divs:
-            # Remove unnecessary spaces and linebreaks
             text = " ".join(editable_div.text.split())
             div_id = editable_div['id']
             div_text = text
@@ -91,12 +29,45 @@ class HtmlParser:
             else:
                 self.div_dict[parent_id] = {'text': '', 'children': [div_id]}
 
+        return self.div_dict
+
+
+class MindmapConverter:
+    def __init__(self, graph):
+        self.graph = graph
+        self.temp_file = self.create_temp_file()
+        self.title = self.graph['title']['text']
+
+    def dfs(self, node_id, depth=0, is_root=False, is_title=True):
+        node = self.graph[node_id]
+        text = node['text']
+        with open(self.temp_file, "a", encoding='utf-8') as f:
+            if is_title:
+                f.write(f"# {self.title}\n\n")
+            if is_root:
+                if node_id == self.graph['root']['children'][0]:
+                    f.write(f"## {text}\n\n")
+                if node_id in self.graph['root']['children'][1:]:
+                    f.write(f"\n## {text}\n\n")
+            elif text != '':
+                indent = "  " * (depth - 2)
+                f.write(f"{indent}- {text}\n")
+        for child_id in node['children']:
+            self.dfs(child_id, depth + 1, is_root=(node_id == 'root'), is_title=False)
+
+    def create_temp_file(self):
+        return tempfile.NamedTemporaryFile(mode='w', delete=False).name
+
+    def convert(self):
+        self.dfs('root')
+        shutil.move(self.temp_file, f"{self.title}.md")
+
 
 if __name__ == '__main__':
-    parser = HtmlParser('page')
-    parser.parse()
-    converter = HTMLMindmaptoMDConverter('page')
+    html_parser = HtmlParser('page')
+    div_dict = html_parser.parse()
+    converter = MindmapConverter(div_dict)
     converter.convert()
 
     # Print the adjacency list
-    [print(key, ':', value) for key, value in parser.div_dict.items()]
+    # [print(key, ':', value) for key, value in div_dict.items()]

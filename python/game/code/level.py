164b86b49e3fb85e8e00302607d132a7e35fc909
Vinícius Amorim <vamorim.dev@gmail.com>
2023-04-01 10:19:00 -0300

    feat: add camera movement to the game

--- python/game/code/level.py
+++ python/game/code/level.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 import pygame
 import settings as s
-from debug import debug
 from player import Player
 from tile import Tile
+# from debug import debug
 
 
 class Level():
@@ -12,7 +12,7 @@ class Level():
         # get display surface
         self.display_surface = pygame.display.get_surface()
         # sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstable_sprites = pygame.sprite.Group()
         self.create_map()
 
@@ -27,6 +27,22 @@ class Level():
                     self.player = Player((x, y), [self.visible_sprites], self.obstable_sprites)
 
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(100, 200)
+
+    def custom_draw(self, player: Player):
+        self.offset.x = self.half_width - player.rect.centerx
+        self.offset.y = self.half_height - player.rect.centery
+
+        for sprite in self.sprites():
+            offset_position = sprite.rect.topleft + self.offset
+            self.display_surface.blit(sprite.image, offset_position)

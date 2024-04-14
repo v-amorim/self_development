1c16ad983de4777254713f74202360f1f7fe1478
v-amorim <vamorim.dev@gmail.com>
2024-04-14 20:47:06 -0300

    feat: formatting

--- misc/mpv64/portable_config/scripts/skip-to-silence.lua
+++ misc/mpv64/portable_config/scripts/skip-to-silence.lua
@@ -35,14 +35,14 @@ mutewhileskipping = no
 --]]
 
 local opts = {
-    quietness = -30,
-    duration = 0.1,
-    mutewhileskipping = false
+	quietness = -30,
+	duration = 0.1,
+	mutewhileskipping = false,
 }
 
-local mp = require 'mp'
-local msg = require 'mp.msg'
-local options = require 'mp.options'
+local mp = require("mp")
+local msg = require("mp.msg")
+local options = require("mp.options")
 
 old_speed = 1
 was_paused = false
@@ -60,69 +60,67 @@ Dev note about the used filters:
 --]]
 
 function doSkip()
-    -- Get video dimensions
-    local width = mp.get_property_native("width");
-    local height = mp.get_property_native("height")
+	-- Get video dimensions
+	local width = mp.get_property_native("width")
+	local height = mp.get_property_native("height")
 
-    -- Create audio and video filters
-    mp.command(
-        "no-osd af add @skiptosilence:lavfi=[silencedetect=noise=" ..
-        opts.quietness .. "dB:d=" .. opts.duration .. "]"
-    )
-    mp.command(
-        "no-osd vf add @skiptosilence-blackout:lavfi=" ..
-        "[nullsink,color=c=black:s=" .. width .. "x" .. height .. "]"
-    )
+	-- Create audio and video filters
+	mp.command(
+		"no-osd af add @skiptosilence:lavfi=[silencedetect=noise=" .. opts.quietness .. "dB:d=" .. opts.duration .. "]"
+	)
+	mp.command(
+		"no-osd vf add @skiptosilence-blackout:lavfi=" .. "[nullsink,color=c=black:s=" .. width .. "x" .. height .. "]"
+	)
 
-    -- Triggers whenever the `silencedetect` filter emits output
-    mp.observe_property("af-metadata/skiptosilence", "string", foundSilence)
+	-- Triggers whenever the `silencedetect` filter emits output
+	mp.observe_property("af-metadata/skiptosilence", "string", foundSilence)
 
-    was_muted = mp.get_property_native("mute")
-    if opts.mutewhileskipping then
-        mp.set_property_bool("mute", true)
-    end
+	was_muted = mp.get_property_native("mute")
+	if opts.mutewhileskipping then
+		mp.set_property_bool("mute", true)
+	end
 
-    was_paused = mp.get_property_native("pause")
-    mp.set_property_bool("pause", false)
-    old_speed = mp.get_property_native("speed")
-    mp.set_property("speed", 100)
+	was_paused = mp.get_property_native("pause")
+	mp.set_property_bool("pause", false)
+	old_speed = mp.get_property_native("speed")
+	mp.set_property("speed", 100)
 end
 
 function foundSilence(name, value)
-    if value == "{}" or value == nil then
-        return -- For some reason these are sometimes emitted. Ignore.
-    end
+	if value == "{}" or value == nil then
+		return -- For some reason these are sometimes emitted. Ignore.
+	end
 
-    timecode = tonumber(string.match(value, "%d+%.?%d+"))
-    time_pos = mp.get_property_native("time-pos")
-    if timecode == nil or timecode < time_pos + 1 then
-        return -- Ignore anything less than a second ahead.
-    end
+	timecode = tonumber(string.match(value, "%d+%.?%d+"))
+	time_pos = mp.get_property_native("time-pos")
+	if timecode == nil or timecode < time_pos + 1 then
+		return -- Ignore anything less than a second ahead.
+	end
 
-    mp.set_property_bool("mute", was_muted)
-    mp.set_property_bool("pause", was_paused)
-    mp.set_property("speed", old_speed)
-    mp.unobserve_property(foundSilence)
+	mp.set_property_bool("mute", was_muted)
+	mp.set_property_bool("pause", was_paused)
+	mp.set_property("speed", old_speed)
+	mp.unobserve_property(foundSilence)
 
-    -- Remove used audio and video filters
-    mp.command("no-osd af remove @skiptosilence")
-    mp.command("no-osd vf remove @skiptosilence-blackout")
+	-- Remove used audio and video filters
+	mp.command("no-osd af remove @skiptosilence")
+	mp.command("no-osd vf remove @skiptosilence-blackout")
 
-    -- Seeking to the exact moment even though we've already
-    -- fast forwarded here allows the video decoder to skip
-    -- the missed video. This prevents massive A-V lag.
-    mp.set_property_number("time-pos", timecode)
+	-- Seeking to the exact moment even though we've already
+	-- fast forwarded here allows the video decoder to skip
+	-- the missed video. This prevents massive A-V lag.
+	mp.set_property_number("time-pos", timecode)
 
-    -- If we don't wait at least 50ms before messaging the user, we
-    -- end up displaying an old value for time-pos.
-    mp.add_timeout(0.05, skippedMessage)
+	-- If we don't wait at least 50ms before messaging the user, we
+	-- end up displaying an old value for time-pos.
+	mp.add_timeout(0.05, skippedMessage)
 end
 
 function skippedMessage()
-    msg.info("Skipped to silence at " .. mp.get_property_osd("time-pos"))
-    mp.osd_message("Skipped to silence at " .. mp.get_property_osd("time-pos"))
+	msg.info("Skipped to silence at " .. mp.get_property_osd("time-pos"))
+	mp.osd_message("Skipped to silence at " .. mp.get_property_osd("time-pos"))
 end
 
 options.read_options(opts)
 
-mp.add_key_binding("F3", "skip-to-silence", doSkip)
+mp.add_key_binding("F9", "skip-to-silence", doSkip)

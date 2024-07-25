0ee6606348ecf05a85a6f109e0445e98a0e6d7a3
Vinicius Amorim <vamorim.dev@gmail.com>
2024-07-25 16:49:53 -0300

    refactor: Update v-amorim oh-my-posh theme colors

--- config/oh-my-posh/Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-﻿# Oh My Posh: https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/bubblesextra.omp.json
-# remove repeated ".venv": https://github.com/JanDeDobbeleer/oh-my-posh/discussions/390
-# Parameters https://learn.microsoft.com/en-us/powershell/module/psreadline/set-psreadlineoption?view=powershell-7.4#parameters
-# Copy only the commands, not the comments
-
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/v-amorim/self_development/main/config/oh-my-posh/themes/v-amorim.omp.json' | Invoke-Expression
-$env:VIRTUAL_ENV_DISABLE_PROMPT = 1
-Set-PSReadLineOption -PredictionSource History

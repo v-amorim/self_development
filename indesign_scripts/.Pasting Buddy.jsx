fe402277001ec124c19ae40f1a6afe72eec0c10c
Vinicius Amorim <vamorim.dev@gmail.com>
2025-01-30 18:51:04 -0300

    feat: enhance Pasting Buddy script with improved UI elements and functionality

--- indesign_scripts/.Pasting Buddy.jsx
+++ indesign_scripts/.Pasting Buddy.jsx
@@ -1,14 +1,15 @@
 #targetengine session
 
-// Kind regards to Yuri Khristich for helping me solve this: https://stackoverflow.com/a/69066444
+// Main Dialog Window
 var dialog = new Window("palette", "Pasting Buddy", undefined);
 dialog.preferredSize.width = 300;
 dialog.preferredSize.height = 80;
 dialog.text = "Pasting Buddy";
 dialog.onClose = function() {
-    doc.removeEventListener('afterSelectionChanged', selectionChanged)
-}
+    doc.removeEventListener('afterSelectionChanged', selectionChanged);
+};
 
+// UI Elements
 dialog.add("statictext", undefined, "Previous text:");
 var previousPastedTextEdit = dialog.add("edittext");
 previousPastedTextEdit.characters = 40;
@@ -18,8 +19,7 @@ var nextPasteTextEdit = dialog.add("edittext");
 nextPasteTextEdit.text = "";
 nextPasteTextEdit.characters = 40;
 
-// ACTIONSPANEL
-// ============
+// Actions Panel
 var actionsPanel = dialog.add("panel", undefined, undefined, {
     name: "actionsPanel"
 });
@@ -29,6 +29,7 @@ actionsPanel.alignChildren = ["center", "top"];
 actionsPanel.spacing = 10;
 actionsPanel.margins = 10;
 
+// Pause Button
 var pauseState = false;
 var pauseButton = actionsPanel.add("button", undefined, undefined, {
     name: "pauseButton"
@@ -36,11 +37,11 @@ var pauseButton = actionsPanel.add("button", undefined, undefined, {
 pauseButton.text = "Pause";
 pauseButton.onClick = function() {
     pauseState = !pauseState;
-    if (pauseState) nextPasteTextEdit.text = "[PAUSED] " + nextPasteTextEdit.text;
-    if (!pauseState) nextPasteTextEdit.text = nextPasteTextEdit.text.replace(/^\[PAUSED\] /, "");
+    nextPasteTextEdit.text = pauseState ? "[PAUSED] " + nextPasteTextEdit.text : nextPasteTextEdit.text.replace(/^\[PAUSED\] /, "");
     pauseButton.text = pauseState ? "Resume" : "Pause";
 };
 
+// Formatting Button
 var formattingState = false;
 var formattingButton = actionsPanel.add("button", undefined, undefined, {
     name: "formattingButton"
@@ -51,29 +52,30 @@ formattingButton.onClick = function() {
     formattingButton.text = formattingState ? "Paste without formatting" : "Paste with formatting";
 };
 
+// Remove Empty Lines Button
 var removeEmptyLinesButton = actionsPanel.add("button", undefined, undefined, {
     name: "removeEmptyLinesButton"
 });
 removeEmptyLinesButton.text = "Remove Empty Lines";
-removeEmptyLinesButton.onClick = function() {
-    removeEmptyLinesFromMasterFrame();
-};
+removeEmptyLinesButton.onClick = removeEmptyLinesFromMasterFrame;
 
-
+// Document and Master Frame
 var doc = app.activeDocument;
 var master_frame = app.selection[0]; // <-- the Text Script frame
 
+// Initialization
 function init() {
     if (master_frame instanceof TextFrame || master_frame instanceof Story) {
         dialog.show();
         app.selection = null;
         doc.addEventListener('afterSelectionChanged', selectionChanged);
     } else {
-        alert("Please, select a frame with text before run the script!");
+        alert("Please, select a frame with text before running the script!");
         exit();
     }
 }
 
+// Selection Changed Event Handler
 function selectionChanged() {
     if (pauseState) return;
 
@@ -83,7 +85,7 @@ function selectionChanged() {
     if (sel instanceof TextFrame || sel instanceof Story) {
         if (sel.contents == '' && master_frame.contents != '') {
             app.select(master_frame.paragraphs[0], SelectionOptions.REPLACE_WITH);
-            nextText = master_frame.paragraphs[1] ? master_frame.paragraphs[1].contents : "";
+            var nextText = master_frame.paragraphs[1] ? master_frame.paragraphs[1].contents : "";
             nextPasteTextEdit.text = nextText;
             app.cut();
             app.selection = null;
@@ -96,29 +98,25 @@ function selectionChanged() {
             }
             previousPastedTextEdit.text = sel.contents;
 
-
             app.selection = null;
-
             removeLineBreaks(sel);
-
             sel.parentStory.changeGrep();
         }
 
-        if (master_frame.contents == '') alert("No text to paste anymore!")
+        if (master_frame.contents == '') alert("No text to paste anymore!");
     }
 }
 
-
+// Remove Line Breaks
 function removeLineBreaks(sel) {
-    // remove '\r' at the end
     app.findGrepPreferences.findWhat = "\r$";
     app.changeGrepPreferences.changeTo = "";
     sel.changeGrep();
     sel.parentStory.changeGrep();
 }
 
+// Remove Empty Lines from Master Frame
 function removeEmptyLinesFromMasterFrame() {
-    // remove lines with only '\r' from master_frame
     var story = master_frame.parentStory;
     var paragraphs = story.paragraphs;
 
@@ -129,4 +127,5 @@ function removeEmptyLinesFromMasterFrame() {
     }
 }
 
+// Start the script
 init();
